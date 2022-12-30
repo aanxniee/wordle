3c6f9a4d58c286bf03ad73a11464239574cbbd69
Annie Cai <caishajia@gmail.com>
2022-12-29 21:20:02 -0500

    created random word generator function

--- wordle.cpp
+++ wordle.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-#include <cctype>
 #include <vector>
+#include <curl/curl.h>
+#include "config.h"
 using namespace std;
 
 const int WORD_LENGTH = 5;
+
+// ----------------------------------------------------------------
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+// ----------------------------------------------------------------
+
 
 bool isValid(string s);
 void toUpper(string &s);
@@ -19,23 +31,52 @@ int main () {
 
     string input;
     int currentTry = 0;
+    string word = randomWord();
+
+    cout << word << endl;
 
     while (currentTry < numTries) {
         do {
             cout << "Enter a guess (Q to quit): ";
             cin >> input;
             toUpper(input);
-        } while (input != "Q");
+        } while (input != "Q" && !isValid(word));
 
         if (input == "Q") {
             cout << "Thank you for playing!" << endl;
             break;
         }
+
         currentTry++;
     }
     
 }
 
+bool isValid(string s) {
+    return (s.length() != 5 && s.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") ==
+        std::string::npos);
+}
+
 void toUpper(string &s) {
     transform(s.begin(), s.end(), s.begin(), ::toupper);
+}
+
+string randomWord() {
+    string word;
+    CURL *hnd = curl_easy_init();
+    curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_easy_setopt(hnd, CURLOPT_URL, API::URL);
+
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, API::KEY);
+    headers = curl_slist_append(headers, API::HOST);
+    curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+
+    curl_easy_setopt(hnd, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(hnd, CURLOPT_WRITEDATA, &word);
+
+    CURLcode ret = curl_easy_perform(hnd);
+    curl_easy_cleanup(hnd);
+
+    return word;
 }
