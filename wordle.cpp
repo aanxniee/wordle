56d8a8582f9fd12e6b00fecce850a351f54eb876
Annie Cai <caishajia@gmail.com>
2022-12-29 22:32:40 -0500

    added print function to display correctness of guesses

--- wordle.cpp
+++ wordle.cpp
@@ -27,7 +27,9 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
 bool isValid(string s);
 void toUpper(string &s);
 string randomWord();
-void print(vector<string>tries, vector<vector<int> > macthes, int currentTry);
+void checkMatch(string input, string word, vector<vector<int> > &matches, int currentGuess);
+bool allMatched(string input, string word);
+void print(vector<string>tries, vector<vector<int> > matches, int currentTry);
 
 // ----------------------------------------------------------------
 
@@ -39,16 +41,12 @@ int main () {
     int currentGuess = 0;
     string word = randomWord();
     toUpper(word);
-
     cout << word << endl;
-    print(guesses, matches, currentGuess);
-
     while (currentGuess < MAX_GUESSES) {
-        do {
-            cout << "ENTER A GUESS (Q TO QUIT): ";
-            cin >> input;
-            toUpper(input);
-        } while (input != "Q" && !isValid(word));
+    
+        cout << "ENTER A GUESS (Q TO QUIT): ";
+        cin >> input;
+        toUpper(input);
 
         if (input == "Q") {
             cout << endl;
@@ -58,7 +56,14 @@ int main () {
 
         guesses[currentGuess] = input;
 
+        checkMatch(input, word, matches, currentGuess);
         print(guesses, matches, currentGuess);
+
+        if (allMatched(input, word)) {
+            cout << "YOU WIN!" << endl;
+            break;
+        }
+
         currentGuess++;
     }
     
@@ -95,9 +100,67 @@ string randomWord() {
     return word;
 }
 
+void checkMatch(string input, string word, vector<vector<int> > &matches, int currentGuess) {
+    for (int i = 0; i < WORD_LENGTH; i++) {
+        matches[currentGuess][i] = NOT_MATCHED;
+    }
+
+    for (int i = 0; i < WORD_LENGTH; i++) {
+        for (int j = 0; j < WORD_LENGTH; j++) {
+            if (input[i] == word[j]) {
+                if (i == j) {
+                    matches[currentGuess][i] = MATCHED;
+                    break;
+                }
+                else {
+                    matches[currentGuess][i] = PARTIAL_MATCHED;
+                }
+            }
+        }
+    }
+}
+
+bool allMatched(string input, string word) {
+    return (input == word);
+}
+
 void print(vector<string>guesses, vector<vector<int> > matches, int currentGuess) {
     system("clear");
     cout << " ---------------" << endl;
     cout << "|  W O R D L E  |" << endl ;
     cout << " ---------------" << endl << endl;
+
+    for (int i = 0; i <= currentGuess && i < MAX_GUESSES; i++) {
+
+        string padding = " -";
+        string text = "|";
+
+        for (int j = 0; j < WORD_LENGTH; j++) {
+            padding += "---";
+            text += " ";
+
+            char letter = toupper(guesses[i][j]);
+
+            if (matches[i][j] == PARTIAL_MATCHED) {
+                text += "\033[33m";
+            }
+            else if (matches[i][j] == MATCHED) {
+                text += "\033[32m";
+            }
+
+            text += letter;
+            text += " ";
+
+            if (matches[i][j] == PARTIAL_MATCHED || matches[i][j] == MATCHED) {
+                text += "\033[0m";
+            }
+
+        }
+        if (i == 0) {
+            cout << padding << endl;
+        }
+
+        cout << text << endl;
+        cout << padding << endl;
+    }
 }
